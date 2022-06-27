40ed1d5c1adaf37a2a8ca3ad266d75f6ac3b5a0d
赤川大空 <daisuke1024akagawa@gmail.com>
2022-06-27 12:09:38 +0900

    fix version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 setuptools.setup(
     name="PyConfigParser",
     url="https://github.com/Akasan/PyConfigParser",
-    version="1.0.3",
+    version="1.0.4",
     install_requires=[],
     packages=[
         "ConfigParser",
